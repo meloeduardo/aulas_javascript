d917fa0e71fd300828dfb2b70b7e7d0c22f2a5ee
Eduardo Melo <eduardomelopereira03@gmail.com>
2025-09-03 19:16:34 -0300

    atualizações locais

---
+++ tests/soma.test.js
@@ -0,0 +1,9 @@
+import { soma } from '../src/soma.js';
+
+test('soma 2 + 3 = 5', () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test('conversão de string', () => {
+  expect(soma('10', 1)).toBe(11);
+});
